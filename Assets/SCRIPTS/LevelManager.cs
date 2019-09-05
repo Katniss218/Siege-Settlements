f7a83447fcace2820da684d00dc837d8f769254f
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2019-09-05 15:31:10 +0200

    Slight cleanup.

--- Assets/SCRIPTS/LevelManager.cs
+++ Assets/SCRIPTS/LevelManager.cs
@@ -1,6 +1,7 @@
 ﻿using KFF;
 using SS.Data;
 using SS.Extras;
+using SS.Heroes;
 using SS.TerrainCreation;
 using SS.Units;
 using System.Collections.Generic;
@@ -41,36 +42,74 @@ namespace SS.Levels
 
 		public void Start()
 		{
+			const int size = 4; // the size of the map (in chunks).
+
 			LevelTerrainCreator.terrainParent = this.transform;
-			Texture2D[,] color = new Texture2D[4, 4];
-			for( int i = 0; i < 4; i++ )
+			Texture2D[,] color = new Texture2D[size, size];
+			for( int i = 0; i < size; i++ )
 			{
-				for( int j = 0; j < 4; j++ )
+				for( int j = 0; j < size; j++ )
 				{
-					color[i, j] = Resources.Load<Texture2D>( "colormap/row-" + (4 - j) + "-col-" + (i + 1) );
+					color[i, j] = Resources.Load<Texture2D>( "colormap/row-" + (size - j) + "-col-" + (i + 1) );
 				}
 			}
-			Texture2D[,] height = new Texture2D[4, 4];
-			for( int i = 0; i < 4; i++ )
+			Texture2D[,] height = new Texture2D[size, size];
+			for( int i = 0; i < size; i++ )
 			{
-				for( int j = 0; j < 4; j++ )
+				for( int j = 0; j < size; j++ )
 				{
-					height[i, j] = Resources.Load<Texture2D>( "heightmap/row-" + (4 - j) + "-col-" + (i + 1) );
+					height[i, j] = Resources.Load<Texture2D>( "heightmap/row-" + (size - j) + "-col-" + (i + 1) );
 				}
 			}
 			LevelTerrainCreator.SpawnMap( height, color, 6f );
 			LevelTerrainCreator.UpdateNavMesh();
 
 			Main.cameraPivot.position = new Vector3( 32, 0, 32 );
+
 			List<UnitDefinition> units = DataManager.GetAllOfType<UnitDefinition>();
 			for( int i = 0; i < units.Count; i++ )
 			{
 				for( int j = 0; j < 4; j++ )
 				{
-					Unit.Create( units[i], new Vector3( Random.Range( 22f, 42f ), 0, Random.Range( 32 + i, 32 + (2 * i) ) ), Quaternion.identity, 0 );
-					Unit.Create( units[i], new Vector3( Random.Range( 22f, 42f ), 0, Random.Range( 32 + (-2 * i), 32 + (-i) ) ), Quaternion.identity, 1 );
+					float x = Random.Range( 22f, 42f );
+					float z = Random.Range( 32 + i, 32 + (2 * i) );
+
+					if( Physics.Raycast( new Vector3( x, 50f, z ), Vector3.down, out RaycastHit hit, 100f ) )
+					{
+						Unit.Create( units[i], hit.point, Quaternion.identity, 0 );
+					}
+					x = Random.Range( 22f, 42f );
+					z = Random.Range( 32 + (-2 * i), 32 + (-i) );
+
+					if( Physics.Raycast( new Vector3( x, 50f, z ), Vector3.down, out hit, 100f ) )
+					{
+						Unit.Create( units[i], hit.point, Quaternion.identity, 1 );
+					}
 				}
 			}
+
+			List<HeroDefinition> heroes = DataManager.GetAllOfType<HeroDefinition>();
+			for( int i = 0; i < heroes.Count; i++ )
+			{
+				for( int j = 0; j < 4; j++ )
+				{
+					float x = Random.Range( 22f, 42f );
+					float z = Random.Range( 32 + i, 32 + (2 * i) );
+
+					if( Physics.Raycast( new Vector3( x, 50f, z ), Vector3.down, out RaycastHit hit, 100f ) )
+					{
+						Hero.Create( heroes[i], hit.point, Quaternion.identity, 0 );
+					}
+					x = Random.Range( 22f, 42f );
+					z = Random.Range( 32 + (-2 * i), 32 + (-i) );
+
+					if( Physics.Raycast( new Vector3( x, 50f, z ), Vector3.down, out hit, 100f ) )
+					{
+						Hero.Create( heroes[i], hit.point, Quaternion.identity, 1 );
+					}
+				}
+			}
+
 
 			List<ExtraDefinition> extras = DataManager.GetAllOfType<ExtraDefinition>();
 			for( int i = 0; i < extras.Count; i++ )
