32843ca12e60fdee7276df6b3fb8e136baa93e96
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2019-09-05 15:54:03 +0200

    Added heroes.

---
+++ Assets/SCRIPTS/03 Heroes/Hero.cs
@@ -0,0 +1,172 @@
+﻿using SS.Data;
+using SS.Projectiles;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace SS.Heroes
+{
+	public class Hero : MonoBehaviour
+	{
+		/// <summary>
+		/// Contains all of the original values for this hero. Might be not accurate to the overriden values on GameObjects (Read Only).
+		/// </summary>
+		public HeroDefinition cachedDefinition { get; private set; }
+
+
+		// Start is called before the first frame update
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		public static GameObject Create( HeroDefinition def, Vector3 pos, Quaternion rot, int factionId )
+		{
+			if( def == null )
+			{
+				throw new System.Exception( "Definition can't be null" );
+			}
+			GameObject container = new GameObject( "Hero (\"" + def.id + "\"), (f: " + factionId + ")" );
+			container.layer = LayerMask.NameToLayer( "Units" );
+
+			GameObject gfx = new GameObject( "graphics" );
+			gfx.transform.SetParent( container.transform );
+
+			container.transform.SetPositionAndRotation( pos, rot );
+
+			// Add a mesh to the unit.
+			MeshFilter meshFilter = gfx.AddComponent<MeshFilter>();
+			meshFilter.mesh = def.mesh.Item2;
+
+			// Add a material to the unit.
+			MeshRenderer meshRenderer = gfx.AddComponent<MeshRenderer>();
+			meshRenderer.sharedMaterial = Main.materialFactionColoredDestroyable;
+			// Set the material's properties to the appropriate values.
+			meshRenderer.material.SetColor( "_FactionColor", FactionManager.factions[factionId].color );
+			meshRenderer.material.SetTexture( "_Albedo", def.albedo.Item2 );
+			meshRenderer.material.SetTexture( "_Normal", def.normal.Item2 );
+			meshRenderer.material.SetTexture( "_Emission", null );
+			meshRenderer.material.SetFloat( "_Metallic", 0.0f );
+			meshRenderer.material.SetFloat( "_Smoothness", 0.5f );
+
+			// Assign the definition to the unit, so it can be accessed later.
+			Hero hero = container.AddComponent<Hero>();
+			hero.cachedDefinition = def;
+
+			BoxCollider collider = container.AddComponent<BoxCollider>();
+			collider.size = new Vector3( def.radius * 2.0f, def.height, def.radius * 2.0f );
+			collider.center = new Vector3( 0.0f, def.height / 2.0f, 0.0f );
+
+			// Mask the unit as selectable.
+			Selectable selectable = container.AddComponent<Selectable>();
+			selectable.icon = def.icon.Item2;
+			
+			// Add a kinematic rigidbody to the unit (required by the NavMeshAgent).
+			Rigidbody rigidbody = container.AddComponent<Rigidbody>();
+			rigidbody.isKinematic = true;
+
+			// Add the NavMeshAgent to the unit, to make it movable.
+			NavMeshAgent navMeshAgent = container.AddComponent<NavMeshAgent>();
+			navMeshAgent.acceleration = 8.0f;
+			navMeshAgent.stoppingDistance = 0.125f;
+			navMeshAgent.radius = def.radius;
+			navMeshAgent.height = def.height;
+			navMeshAgent.speed = def.movementSpeed;
+			navMeshAgent.angularSpeed = def.rotationSpeed;
+			
+			// Make the unit belong to a faction.
+			FactionMember factionMember = container.AddComponent<FactionMember>();
+			factionMember.onFactionChange.AddListener( () =>
+			{
+				Color color = FactionManager.factions[factionMember.factionId].color;
+				//ui.SetFactionColor( color );
+				meshRenderer.material.SetColor( "_FactionColor", color );
+			} );
+			// We set the faction after assigning the listener, to automatically set the color to the appropriate value.
+			factionMember.factionId = factionId;
+
+			// Make the unit damageable.
+			Damageable damageable = container.AddComponent<Damageable>();
+			damageable.onHealthChange.AddListener( () =>
+			{
+				meshRenderer.material.SetFloat( "_Dest", 1 - damageable.healthPercent );
+				//ui.SetHealthFill( damageable.healthPercent );
+			} );
+			// Make the unit deselect itself, and destroy it's UI when killed.
+			damageable.onDeath.AddListener( () =>
+			{
+				//Object.Destroy( ui.gameObject );
+				// for breakup make several meshes that are made up of the original one, attach physics to them.
+				// let the physics play for a few seconds (randomize durations for each piece), then disable rigidbodies, and pull them downwards, reducing their scale at the same time.
+				// when the scale reaches 0.x, remove the piece.
+
+				// also, play a poof from some particle system for smoke or something at the moment of death.
+				if( SelectionManager.IsSelected( selectable ) )
+				{
+					SelectionManager.Deselect( selectable ); // We have all of the references of this unit here, so we can just simply pass it like this. Amazing, right?
+				}
+
+			} );
+			damageable.healthMax = def.healthMax;
+			damageable.Heal();
+			damageable.armor = def.armor;
+			
+			// If the unit has the capability to fight, add a target finder to it.
+			ITargetFinder finder = null;
+			if( def.isMelee || def.isRanged )
+			{
+				finder = container.AddComponent<TargetFinderModule>();
+			}
+
+			// If the new unit is melee, setup the melee module.
+			if( def.isMelee )
+			{
+				DamageSource meleeDamageSource = container.AddComponent<DamageSource>();
+				meleeDamageSource.damageType = def.meleeDamageType;
+				meleeDamageSource.damage = def.meleeDamage;
+				meleeDamageSource.armorPenetration = def.meleeArmorPenetration;
+
+				MeleeModule melee = container.AddComponent<MeleeModule>();
+				melee.damageSource = meleeDamageSource;
+				melee.targetFinder = finder;
+				melee.attackCooldown = def.meleeAttackCooldown;
+				melee.attackRange = def.meleeAttackRange;
+				melee.attackSoundEffect = def.meleeAttackSoundEffect.Item2;
+			}
+
+			// If the new unit is ranged, setup the ranged module.
+			if( def.isRanged )
+			{
+				DamageSource rangedDamageSource = container.AddComponent<DamageSource>();
+				rangedDamageSource.damageType = def.rangedDamageType;
+				rangedDamageSource.damage = def.rangedDamage;
+				rangedDamageSource.armorPenetration = def.rangedArmorPenetration;
+
+				RangedModule ranged = container.AddComponent<RangedModule>();
+				ranged.projectile = DataManager.Get<ProjectileDefinition>( def.rangedProjectileId );
+				ranged.projectileCount = def.rangedProjectileCount;
+				ranged.damageSource = rangedDamageSource;
+				ranged.targetFinder = finder;
+				ranged.attackRange = def.rangedAttackRange;
+				ranged.attackCooldown = def.rangedAttackCooldown;
+				ranged.velocity = def.rangedVelocity;
+				ranged.localOffsetMin = def.rangedLocalOffsetMin;
+				ranged.localOffsetMax = def.rangedLocalOffsetMax;
+				ranged.attackSoundEffect = def.rangedAttackSoundEffect.Item2;
+			}
+
+			// Make the unit update it's UI's position every frame.
+			/*container.AddComponent<EveryFrameSingle>().onUpdate = () =>
+			{
+				ui.transform.position = Main.camera.WorldToScreenPoint( container.transform.position );
+			};*/
+
+			return container;
+		}
+	}
+}
---
+++ Assets/SCRIPTS/03 Heroes/HeroDefinition.cs
@@ -0,0 +1,159 @@
+﻿using Katniss.Utils;
+using KFF;
+using SS.Data;
+using System;
+using UnityEngine;
+
+namespace SS.Heroes
+{
+	public class HeroDefinition : Definition
+	{
+		public string displayName { get; set; }
+
+
+		// Health-related
+		public float healthMax { get; set; }
+
+		public Armor armor { get; set; }
+
+
+		// Melee-related
+		public bool isMelee { get; set; }
+		public DamageType meleeDamageType { get; set; }
+		public float meleeDamage { get; set; }
+		public float meleeArmorPenetration { get; set; }
+		public float meleeAttackRange { get; set; }
+		public float meleeAttackCooldown { get; set; }
+		public Tuple<string, AudioClip> meleeAttackSoundEffect { get; private set; }
+
+		// Ranged-related
+		public bool isRanged { get; set; }
+		public string rangedProjectileId { get; set; }
+		public int rangedProjectileCount { get; set; }
+		public DamageType rangedDamageType { get; set; }
+		public float rangedDamage { get; set; }
+		public float rangedArmorPenetration { get; set; }
+		public float rangedAttackRange { get; set; }
+		public float rangedAttackCooldown { get; set; }
+		public float rangedVelocity { get; set; }
+		public Vector3 rangedLocalOffsetMin { get; set; }
+		public Vector3 rangedLocalOffsetMax { get; set; }
+		public Tuple<string, AudioClip> rangedAttackSoundEffect { get; private set; }
+
+
+		// Movement-related
+		public float movementSpeed { get; set; }
+		public float rotationSpeed { get; set; }
+
+		public float radius { get; set; }
+		public float height { get; set; }
+
+		// Display-related
+		public Tuple<string, Mesh> mesh { get; private set; }
+		public Tuple<string, Texture2D> albedo { get; private set; }
+		public Tuple<string, Texture2D> normal { get; private set; }
+		public Tuple<string, Sprite> icon { get; private set; }
+
+		public HeroDefinition( string id ) : base( id )
+		{
+
+		}
+
+		public override void DeserializeKFF( KFFSerializer serializer )
+		{
+			this.id = serializer.ReadString( "Id" );
+			this.displayName = serializer.ReadString( "DisplayName" );
+
+			this.healthMax = serializer.ReadFloat( "MaxHealth" );
+			this.armor = new Armor();
+			serializer.Deserialize( "Armor", this.armor );
+
+			var analysisData = serializer.Analyze( "MeleeModule" );
+			if( analysisData.isSuccess )
+			{
+				this.isMelee = true;
+				this.meleeDamageType = (DamageType)serializer.ReadByte( "MeleeModule.DamageType" );
+				this.meleeDamage = serializer.ReadFloat( "MeleeModule.Damage" );
+				this.meleeArmorPenetration = serializer.ReadFloat( "MeleeModule.ArmorPenetration" );
+				this.meleeAttackRange = serializer.ReadFloat( "MeleeModule.AttackRange" );
+				this.meleeAttackCooldown = serializer.ReadFloat( "MeleeModule.AttackCooldown" );
+
+				this.meleeAttackSoundEffect = serializer.ReadAudioClipFromAssets( "MeleeModule.AttackSound" );
+			}
+			analysisData = serializer.Analyze( "RangedModule" );
+			if( analysisData.isSuccess )
+			{
+				this.isRanged = true;
+				this.rangedProjectileId = serializer.ReadString( "RangedModule.ProjectileId" );
+				this.rangedProjectileCount = serializer.ReadInt( "RangedModule.ProjectileCount" );
+				this.rangedDamageType = (DamageType)serializer.ReadByte( "RangedModule.DamageType" );
+				this.rangedDamage = serializer.ReadFloat( "RangedModule.Damage" );
+				this.rangedArmorPenetration = serializer.ReadFloat( "RangedModule.ArmorPenetration" );
+				this.rangedAttackRange = serializer.ReadFloat( "RangedModule.AttackRange" );
+				this.rangedAttackCooldown = serializer.ReadFloat( "RangedModule.AttackCooldown" );
+				this.rangedVelocity = serializer.ReadFloat( "RangedModule.Velocity" );
+				this.rangedLocalOffsetMin = serializer.ReadVector3( "RangedModule.LocalOffsetMin" );
+				this.rangedLocalOffsetMax = serializer.ReadVector3( "RangedModule.LocalOffsetMax" );
+
+				this.rangedAttackSoundEffect = serializer.ReadAudioClipFromAssets( "RangedModule.AttackSound" );
+			}
+
+			this.movementSpeed = serializer.ReadFloat( "MovementSpeed" );
+			this.rotationSpeed = serializer.ReadFloat( "RotationSpeed" );
+			this.radius = serializer.ReadFloat( "Radius" );
+			this.height = serializer.ReadFloat( "Height" );
+
+			this.mesh = serializer.ReadMeshFromAssets( "Mesh" );
+			this.albedo = serializer.ReadTexture2DFromAssets( "AlbedoTexture", TextureType.Albedo );
+			this.normal = serializer.ReadTexture2DFromAssets( "NormalTexture", TextureType.Normal );
+			this.icon = serializer.ReadSpriteFromAssets( "Icon" );
+		}
+
+		public override void SerializeKFF( KFFSerializer serializer )
+		{
+			serializer.WriteString( "", "Id", this.id );
+			serializer.WriteString( "", "DisplayName", this.displayName );
+
+			serializer.WriteFloat( "", "MaxHealth", this.healthMax );
+			serializer.Serialize( "", "Armor", this.armor );
+
+			if( this.isMelee )
+			{
+				serializer.WriteClass( "", "MeleeModule" );
+				serializer.WriteByte( "MeleeModule", "DamageType", (byte)this.meleeDamageType );
+				serializer.WriteFloat( "MeleeModule", "Damage", this.meleeDamage );
+				serializer.WriteFloat( "MeleeModule", "ArmorPenetration", this.meleeArmorPenetration );
+				serializer.WriteFloat( "MeleeModule", "AttackRange", this.meleeAttackRange );
+				serializer.WriteFloat( "MeleeModule", "AttackCooldown", this.meleeAttackCooldown );
+
+				serializer.WriteString( "MeleeModule", "AttackSound", this.meleeAttackSoundEffect.Item1 );
+			}
+			if( this.isRanged )
+			{
+				serializer.WriteClass( "", "RangedModule" );
+				serializer.WriteString( "RangedModule", "ProjectileId", this.rangedProjectileId );
+				serializer.WriteInt( "RangedModule", "ProjectileCount", this.rangedProjectileCount );
+				serializer.WriteByte( "RangedModule", "DamageType", (byte)this.rangedDamageType );
+				serializer.WriteFloat( "RangedModule", "Damage", this.rangedDamage );
+				serializer.WriteFloat( "RangedModule", "ArmorPenetration", this.rangedArmorPenetration );
+				serializer.WriteFloat( "RangedModule", "AttackRange", this.rangedAttackRange );
+				serializer.WriteFloat( "RangedModule", "AttackCooldown", this.rangedAttackCooldown );
+				serializer.WriteFloat( "RangedModule", "Velocity", this.rangedVelocity );
+				serializer.WriteVector3( "RangedModule", "LocalOffsetMin", this.rangedLocalOffsetMin );
+				serializer.WriteVector3( "RangedModule", "LocalOffsetMax", this.rangedLocalOffsetMax );
+
+				serializer.WriteString( "RangedModule", "AttackSound", this.rangedAttackSoundEffect.Item1 );
+			}
+
+			serializer.WriteFloat( "", "MovementSpeed", this.movementSpeed );
+			serializer.WriteFloat( "", "RotationSpeed", this.rotationSpeed );
+			serializer.WriteFloat( "", "Radius", this.radius );
+			serializer.WriteFloat( "", "Height", this.height );
+
+			serializer.WriteString( "", "Mesh", this.mesh.Item1 );
+			serializer.WriteString( "", "AlbedoTexture", this.albedo.Item1 );
+			serializer.WriteString( "", "NormalTexture", this.normal.Item1 );
+			serializer.WriteString( "", "Icon", this.icon.Item1 );
+		}
+	}
+}
