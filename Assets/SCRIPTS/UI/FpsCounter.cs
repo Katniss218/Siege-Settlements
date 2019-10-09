4ed558b30921d967a7683a07c8438d56d49874c2
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2019-10-09 07:29:42 +0200

    FpsCounter now properly handles pausing - displays 0 fps, instead of int.MinValue.

--- Assets/SCRIPTS/UI/FpsCounter.cs
+++ Assets/SCRIPTS/UI/FpsCounter.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 namespace SS.UI
 {
 	/// <summary>
-	/// Counts the FPS and displays that to a text field (TextMesh Pro).
+	/// Calculates the FPS and displays it in a text field (TextMesh Pro).
 	/// </summary>
 	public class FpsCounter : MonoBehaviour
 	{
 		[SerializeField] private TMP_Text textField = null;
 
+		/// <summary>
+		/// Tells the FpsCounter to format the text accordingly.
+		/// </summary>
 		public string format = "Fps: {0}";
 
 		private int GetFps()
 		{
+			// If is paused, return 0 FPS.
+			if( Time.deltaTime == 0 )
+			{
+				return 0;
+			}
+			// Otherwise, calculate the real FPS.
 			return Mathf.CeilToInt( 1.0f / Time.deltaTime );
 		}
 
