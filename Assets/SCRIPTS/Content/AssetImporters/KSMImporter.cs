ce586c65aabf8dcefb54879bfb73c11b5a5918c0
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2019-10-26 23:16:28 +0200

    Cleaned up KSM importer slightly.

--- Assets/SCRIPTS/Content/AssetImporters/KSMImporter.cs
+++ Assets/SCRIPTS/Content/AssetImporters/KSMImporter.cs
@@ -32,23 +32,23 @@ namespace SS
 
 			for( int i = 0; i < vertexCount; i++ )
 			{
-				float vx = BytesToFloat32( bytes, pos );
+				float vx = BytesToFloat32LE( bytes, pos );
 				pos += 4;
-				float vy = BytesToFloat32( bytes, pos );
+				float vy = BytesToFloat32LE( bytes, pos );
 				pos += 4;
-				float vz = BytesToFloat32( bytes, pos );
-				pos += 4;
-
-				float nx = BytesToFloat32( bytes, pos );
-				pos += 4;
-				float ny = BytesToFloat32( bytes, pos );
-				pos += 4;
-				float nz = BytesToFloat32( bytes, pos );
+				float vz = BytesToFloat32LE( bytes, pos );
 				pos += 4;
 
-				float u = BytesToFloat32( bytes, pos );
+				float nx = BytesToFloat32LE( bytes, pos );
 				pos += 4;
-				float v = BytesToFloat32( bytes, pos );
+				float ny = BytesToFloat32LE( bytes, pos );
+				pos += 4;
+				float nz = BytesToFloat32LE( bytes, pos );
+				pos += 4;
+
+				float u = BytesToFloat32LE( bytes, pos );
+				pos += 4;
+				float v = BytesToFloat32LE( bytes, pos );
 				pos += 4;
 
 				vertices[i] = new Vector3( vx, vy, vz );
@@ -71,11 +71,11 @@ namespace SS
 
 			for( int i = 0; i < faceCount; i++ )
 			{
-				indices[i * 3] = BytesToInt32( bytes, pos ) - 1;
+				indices[i * 3] = BytesToInt32LE( bytes, pos ) - 1;
 				pos += 4;
-				indices[(i * 3) + 1] = BytesToInt32( bytes, pos ) - 1;
+				indices[(i * 3) + 1] = BytesToInt32LE( bytes, pos ) - 1;
 				pos += 4;
-				indices[(i * 3) + 2] = BytesToInt32( bytes, pos ) - 1;
+				indices[(i * 3) + 2] = BytesToInt32LE( bytes, pos ) - 1;
 				pos += 4;
 			}
 			Mesh mesh = new Mesh();
@@ -91,19 +91,14 @@ namespace SS
 			return mesh;
 		}
 
-		// Convert 4 bytes to an Int32.
-		private static int BytesToInt32( byte[] bytes, int offset = 0 )
+		// Convert 4 bytes to an Int32 (little endian).
+		private static int BytesToInt32LE( byte[] bytes, int offset = 0 )
 		{
 			int value = (bytes[offset]) | (bytes[offset + 1] << 8) | (bytes[offset + 2] << 16) | bytes[offset + 3] << 24;
-			/*int value = 0;
-			for( int i = 0; i < 4; i++ )
-			{
-				value |= (bytes[offset + i]) << (i * 8); // little endian
-			}*/
 			return value;
 		}
 
-		// Convert 4 bytes to an Int32.
+		// Convert 4 bytes to an Int32 (big endian).
 		private static int BytesToInt32BE( byte[] bytes, int offset = 0 )
 		{
 			int value = (bytes[offset] << 24) | (bytes[offset+1] << 16) | (bytes[offset + 2] << 8) | bytes[offset + 3];
@@ -119,11 +114,12 @@ namespace SS
 			[FieldOffset( 0 )]
 			public int intValue;
 		}
-		
-		private static float BytesToFloat32( byte[] bytes, int offset = 0 )
+
+		// Convert 4 bytes to an Float32 (little endian).
+		private static float BytesToFloat32LE( byte[] bytes, int offset = 0 )
 		{
 			IntFloat fl = new IntFloat();
-			fl.intValue = BytesToInt32( bytes, offset );
+			fl.intValue = BytesToInt32LE( bytes, offset );
 			return fl.floatValue;
 		}
 	}
